1426d2e312b77c23497909662f72155602f7c0aa
Rachael Njeri <rachael.njeri@andela.com>
2019-01-14 10:07:36 +0300

    Add a voting feature (real-time graphql Subscriptions)
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -12,15 +12,12 @@ function post(parent, args, context, info) {
 }
 
 async function signup(parent, args, context, info) {
-  // 1
   const password = await bcrypt.hash(args.password, 10);
-  // 2
+
   const user = await context.prisma.createUser({ ...args, password });
 
-  // 3
   const token = jwt.sign({ userId: user.id }, APP_SECRET);
 
-  // 4
   return {
     token,
     user
@@ -28,13 +25,11 @@ async function signup(parent, args, context, info) {
 }
 
 async function login(parent, args, context, info) {
-  // 1
   const user = await context.prisma.user({ email: args.email });
   if (!user) {
     throw new Error('No such user found');
   }
 
-  // 2
   const valid = await bcrypt.compare(args.password, user.password);
   if (!valid) {
     throw new Error('Invalid password');
@@ -42,15 +37,32 @@ async function login(parent, args, context, info) {
 
   const token = jwt.sign({ userId: user.id }, APP_SECRET);
 
-  // 3
   return {
     token,
     user
   };
 }
 
+async function vote(parent, args, context, info) {
+  const userId = getUserId(context);
+
+  const linkExists = await context.prisma.$exists.vote({
+    user: { id: userId },
+    link: { id: args.linkId }
+  });
+  if (linkExists) {
+    throw new Error(`Already voted for link: ${args.linkId}`);
+  }
+
+  return context.prisma.createVote({
+    user: { connect: { id: userId } },
+    link: { connect: { id: args.linkId } }
+  });
+}
+
 module.exports = {
   signup,
   login,
-  post
+  post,
+  vote
 };
