dcfa8b9d2500c52561c8e33dc26cac8b70483d94
Rachael Njeri <rachael.njeri@andela.com>
2019-01-16 11:40:00 +0300

    Schema definition and resolver function implementation
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- blogr/src/index.js
+++ blogr/src/index.js
@@ -1,19 +1,43 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-type Query {
-  description: String
-}
-`;
+let idCount = 0;
+const posts = [];
 
 const resolvers = {
   Query: {
-    description: () => `This is the API for a simple blogging application`
+    description: () => `This is the API for a simple blogging application`,
+    posts: () => posts,
+    post: (parent, args) => posts.find(post => post.id === args.id)
+  },
+  Mutation: {
+    createDraft: (parent, args) => {
+      const post = {
+        id: `post_${idCount++}`,
+        title: args.title,
+        content: args.content,
+        published: false
+      };
+      posts.push(post);
+      return post;
+    },
+    deletePost: (parent, args) => {
+      const postIndex = posts.findIndex(post => post.id === args.id);
+      if (postIndex > -1) {
+        const deleted = posts.splice(postIndex, 1);
+        return deleted[0];
+      }
+      return null;
+    },
+    publish: (parent, args) => {
+      const postIndex = posts.findIndex(post => post.id === args.id);
+      posts[postIndex].published = true;
+      return posts[postIndex];
+    }
   }
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 });
 
